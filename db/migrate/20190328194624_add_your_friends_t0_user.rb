086f9a945243c39426f52518363cb53ba90d633e
Kaleb Allen <7kaleb29allen@gmail.com>
2019-03-28 13:52:21 -0600

    create model controller, routes done

---
+++ db/migrate/20190328194624_add_your_friends_t0_user.rb
@@ -0,0 +1,4 @@
+class AddYourFriendsT0User < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_27_200858) do
+ActiveRecord::Schema.define(version: 2019_03_28_194624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
